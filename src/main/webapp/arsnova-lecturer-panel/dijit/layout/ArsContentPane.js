b99aad1a2ca64e9d3f4ad27708eebf526fda0dda
Daniel Gerhardt <daniel.gerhardt@mni.thm.de>
2013-02-21 00:29:55 +0100

    Automatically resize charts. Added ArsContentPane widget which is able to a resize event.

---
+++ src/main/webapp/arsnova-lecturer-panel/dijit/layout/ArsContentPane.js
@@ -0,0 +1,25 @@
+define(
+	[
+		"dojo/_base/declare",
+		"dojo/Evented",
+		"dijit/layout/ContentPane"
+	],
+	function(declare, Evented, ContentPane) {
+		console.debug("declare");
+		return declare("ArsContentPane", [Evented, ContentPane], {
+			constructor: function() {
+				this.inherited(arguments);
+			},
+			
+			resize: function() {
+				this.inherited(arguments);
+				this.emit("resize", {
+					bubbles: false,
+					cancelable: false,
+					w: this.w,
+					h: this.h
+				});
+			}
+		});
+	}
+);
