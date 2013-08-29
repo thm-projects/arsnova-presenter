0cde6b242ffb3c1be3eeab8e972e5291371a9168
Daniel Gerhardt <daniel.gerhardt@mni.thm.de>
2013-08-29 20:35:13 +0200

    Fix typos in German localization

--- src/main/websources/lib/thm.de/arsnova/presenter/ui/nls/de/main.js
+++ src/main/websources/lib/thm.de/arsnova/presenter/ui/nls/de/main.js
@@ -1,6 +1,6 @@
 define({
-	lowResInfo: "Diese Anwendung kann aufgrund einer zu geringen Anzeigefläche nicht dargestellt werden. ARSnova Presenter ist für Notebooks, Tablets and Arbeitsplatzgeräte optimiert. Sollten Sie ein Tablet nutzen und diese Mitteilung sehen, versuchen Sie es bitte mit waagerechter Ausrichtung.",
-	smallWindowInfo: "Diese Anwendung kann aufgrund einer zu geringen Anzeigefläche nicht dargestellt werden. Bitte vergößern Sie das Fenster Ihres Browsers oder veringern Sie den Zoomfaktor (falls eine Vergrößerung aktiv ist).",
+	lowResInfo: "Diese Anwendung kann aufgrund einer zu geringen Anzeigefläche nicht dargestellt werden. ARSnova Presenter ist für Notebooks, Tablets und Arbeitsplatzgeräte optimiert. Sollten Sie ein Tablet nutzen und diese Mitteilung sehen, versuchen Sie es bitte mit waagerechter Ausrichtung.",
+	smallWindowInfo: "Diese Anwendung kann aufgrund einer zu geringen Anzeigefläche nicht dargestellt werden. Bitte vergößern Sie das Fenster Ihres Browsers oder verringern Sie den Zoomfaktor (falls eine Vergrößerung aktiv ist).",
 	fullScreen: "Vollbild",
 	mode: "Modus",
 	editing: "Bearbeiten",
