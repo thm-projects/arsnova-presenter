3564e589488e55c61d5f86a6d8fec17c95ab1f9f
Daniel Gerhardt <daniel.gerhardt@mni.thm.de>
2013-08-31 15:32:46 +0200

    Update localizations to begin "Presenter" consistently with a capital letter

--- src/main/websources/lib/thm.de/arsnova/presenter/ui/nls/info.js
+++ src/main/websources/lib/thm.de/arsnova/presenter/ui/nls/info.js
@@ -9,8 +9,8 @@ define({
 		commit: "Commit",
 		buildTime: "Build time",
 		bugReportsInfo: "Please mention the version information above in bug reports.",
-		leadDeveloperInfo: "ARSnova presenter (1.0) was developed by Daniel Gerhardt as project for his bachelor thesis. The project is managed by Prof. Dr. Klaus Quibeldey-Cirkel.",
-		creditsInfo: "We would like to thank the following organizations, project groups and individuals for creating the software which ARSnova presenter or some of it's features are based on."
+		leadDeveloperInfo: "ARSnova Presenter (1.0) was developed by Daniel Gerhardt as project for his bachelor thesis. The project is managed by Prof. Dr. Klaus Quibeldey-Cirkel.",
+		creditsInfo: "We would like to thank the following organizations, project groups and individuals for creating the software which ARSnova Presenter or some of it's features are based on."
 	},
 
 	de: true
