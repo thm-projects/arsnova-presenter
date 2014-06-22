276b7330603528b52e7cefc0a00f287ab171b845
Daniel Gerhardt <daniel.gerhardt@mni.thm.de>
2014-06-22 11:56:18 +0200

    Decrease size of built JS

--- src/main/config/presenter-application.prod.profile.js
+++ src/main/config/presenter-application.prod.profile.js
@@ -72,6 +72,41 @@ var profile = (function () {
 					"version"
 				]
 			}
+		},
+
+		staticHasFeatures: {
+			/* These properties allow Closure compiler to remove unused code from Dojo Toolkit
+			 * and further decrease file size of the build */
+			"config-deferredInstrumentation": 0,
+			"config-dojo-loader-catches": 0,
+			"config-tlmSiblingOfDojo": 1,
+			"dojo-amd-factory-scan": 0,
+			"dojo-combo-api": 0,
+			"dojo-config-api": 1,
+			"dojo-config-require": 0,
+			"dojo-debug-messages": 0,
+			"dojo-dom-ready-api": 1,
+			"dojo-firebug": 0,
+			"dojo-guarantee-console": 0,
+			"dojo-has-api": 1,
+			"dojo-inject-api": 1,
+			"dojo-loader": 0,
+			"dojo-log-api": 0,
+			"dojo-modulePaths": 0,
+			"dojo-moduleUrl": 0,
+			"dojo-publish-privates": 0,
+			"dojo-requirejs-api": 0,
+			"dojo-sniff": 0,
+			"dojo-sync-loader": 0,
+			"dojo-test-sniff": 0,
+			"dojo-timeout-api": 0,
+			"dojo-trace-api": 0,
+			"dojo-undef-api": 0,
+			"dojo-v1x-i18n-Api": 1,
+			"dojo-xhr-factory": 0,
+			"dom": 1,
+			"extend-dojo": 1,
+			"host-browser": 1
 		}
 	};
 })();
