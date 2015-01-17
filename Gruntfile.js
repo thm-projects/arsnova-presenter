193d9477a04802787a5e2a3ab695abaf4909e302
Daniel Gerhardt <code@dgerhardt.net>
2015-01-17 16:23:58 +0100

    Start up proxy for backend with Grunt run task

--- Gruntfile.js
+++ Gruntfile.js
@@ -240,10 +240,34 @@ module.exports = function (grunt) {
 			server: {
 				options: {
 					base: outdir,
+					hostname: "localhost",
 					port: 8081,
 					useAvailablePort: true,
-					open: true
-				}
+					open: true,
+					middleware: function (connect, options) {
+						var proxy = require("grunt-connect-proxy/lib/utils").proxyRequest;
+
+						return [
+							/* Serve static files */
+							connect.static(options.base[0]),
+							/* Make empty directories browsable */
+							connect.directory(options.base[0]),
+							/* Include the proxy first */
+							proxy
+						];
+					}
+				},
+				proxies: [
+					{
+						context: ["/api", "/arsnova-config"],
+						host: "localhost",
+						port: 8080,
+						rewrite: {
+							"^/api": ""
+						},
+						xforward: true
+					}
+				]
 			}
 		},
 
@@ -329,6 +353,7 @@ module.exports = function (grunt) {
 		});
 	});
 
+	grunt.loadNpmTasks("grunt-connect-proxy");
 	grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.loadNpmTasks("grunt-contrib-connect");
 	grunt.loadNpmTasks("grunt-contrib-copy");
@@ -353,6 +378,7 @@ module.exports = function (grunt) {
 	grunt.registerTask("run", [
 		"clean",
 		"build:dojo:dev",
+		"configureProxies:server",
 		"connect",
 		"watch",
 		"clean:tmp"
